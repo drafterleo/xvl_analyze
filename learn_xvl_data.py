ad8c8a03fd1269ecb8d03b48f0ebc5fd6555508e
drafterleo <drafterleo@gmail.com>
2016-03-16 01:11:55 +0300

    9d vectors 2d projections

--- learn_xvl_data.py
+++ learn_xvl_data.py
@@ -4,9 +4,12 @@ from sklearn import neighbors
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn import decomposition
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.tree import DecisionTreeClassifier
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
+import itertools
 
 import parse_xvl as xvl
 from vectorize_xvl_data import make_xvl_color_matrix_vec_data
@@ -29,8 +32,9 @@ def learn_predict(xvl_vec_data):
 
     # classifier = svm.SVC(decision_function_shape='ovo', kernel='linear')
     # classifier = neighbors.KNeighborsClassifier(n_neighbors=5, n_jobs=-1)
-    classifier = LinearDiscriminantAnalysis(solver='svd')
+    # classifier = LinearDiscriminantAnalysis(solver='svd')
     # classifier = GaussianNB()
+    classifier = DecisionTreeClassifier()
     classifier.fit(X, Y)
 
     print(list(classifier.predict(X_tst)))
@@ -38,6 +42,22 @@ def learn_predict(xvl_vec_data):
     print(classifier.fit(X, Y).score(X_tst, Y_tst))
 
     # show_pca_transform(X, Y)
+    show_2D_projections(X, Y)
+
+
+def show_2D_projections(X, Y):
+    surfaces = list(itertools.combinations(list(range(len(X[0]))), 2))
+
+    X_a = np.array(X)
+
+    print(len(surfaces), len(X[0]))
+
+    for pairidx, pair in enumerate(surfaces):
+        plt.subplot(6, 6, pairidx + 1)
+        plt.xlabel(str(pair[0]))
+        plt.ylabel(str(pair[1]))
+        plt.scatter(X_a[:, pair[0]], X_a[:, pair[1]], c=Y)
+    plt.show()
 
 
 def show_pca_transform(X, Y):
@@ -52,12 +72,12 @@ def show_pca_transform(X, Y):
     plt.show()
 
 
-
 def make_and_save_xvl_vec_data(xvl_file, json_file):
     xvl_data = xvl.parse_xvl_color_matrix_file(xvl_file)
     xvl_vec_data = make_xvl_color_matrix_vec_data(xvl_data)
     xvl.save_to_json(xvl_vec_data, json_file)
     return xvl_vec_data
+
 
 if __name__ ==  "__main__":
     # xvl_vec_data = make_and_save_xvl_vec_data("ai_src.xvl", "ai_src_vec.json")
